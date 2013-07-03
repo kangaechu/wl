ab56b9ff82bb1b40fd6f71c8a36e46adf6bcafd5
Satoshi Tanaka <kangae2@gmail.com>
2013-07-03 15:59:18 +0900

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: wl
+# Attributes:: default
+#
+# Copyright 2013, kangaechu.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["wl"]["arch"] = "x86_64"
+
+default["wl"]["database"]["selection"] = "mysql"
+default["wl"]["database"]["preinstalled"] = "true"
+
+default["wl"]["mysql"]["host"] = "localhost"
+default["wl"]["mysql"]["port"] = 3306
+default["wl"]["mysql"]["username"] = "worklight"
+default["wl"]["mysql"]["password2"] = "worklight"
+default["wl"]["mysql"]["driver"] = "mysql-connector-java-5.1.25-bin.jar"
+default["wl"]["mysql"]["appcenter"]["username"] = "appcenter"
+default["wl"]["mysql"]["appcenter"]["password"] = "appcenter"
+default["wl"]["mysql"]["appcenter"]["dbname"] = "APPCNTR"
+
+default["wl"]["appserver"]["selection"] = "tomcat"
+default["wl"]["tomcat"]["installdir"] = "/usr/local/tomcat/base"
---
+++ recipes/default.rb
@@ -0,0 +1,98 @@
+#
+# Cookbook Name:: wl
+# Recipe:: default
+#
+# Copyright 2013, kangaechu.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+filenameInstMgr = "agent.installer.linux.gtk.x86_64_1.6.3001.20130528_1750.zip"
+filenameWLServer = "CIN04EN_001.zip"
+InstallationManagerExtractDir = "InstallationManager"
+
+package "unzip" do
+  action :install
+end
+
+bash "Install Installation Manager" do
+  user "root"
+  cwd "/tmp"
+  flags "-x -e"
+  code <<-EOH
+    INSTDATE=`date "+%Y%m%d-%H%M"`
+    rm -rf /tmp/#{InstallationManagerExtractDir}
+    mkdir #{InstallationManagerExtractDir}
+    unzip -q #{Chef::Config[:file_cache_path]}/#{filenameInstMgr} -d #{InstallationManagerExtractDir}
+    cd #{InstallationManagerExtractDir}
+    ./install --launcher.ini silent-install.ini -log /tmp/InstallationManager-${INSTDATE}.log -acceptLicense
+    rm -rf /tmp/#{filenameInstMgr.sub(".zip", "")}
+  EOH
+  not_if {File.exists?("/opt/IBM/InstallationManager")}
+end
+
+template "/tmp/responseFile.xml" do
+  source "responseFile.xml.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/tmp/createdb-mysql.sql" do
+  source "createdb-mysql.sql.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+bash "Create Worklight Databases" do
+  user "root"
+  cwd "/tmp"
+  flags "-x"
+  code <<-EOH
+    mysql -u root < /tmp/createdb-mysql.sql
+    rm -f /tmp/createdb-mysql.sql
+  EOH
+  not_if {File.exists?("/opt/IBM/Worklight")}
+end
+
+bash "Install Worklight Server" do
+  user "root"
+  cwd "/tmp"
+  flags "-x -e"
+  code <<-EOH
+    INSTDATE=`date "+%Y%m%d-%H%M"`
+    cp -f #{Chef::Config[:file_cache_path]}/#{node[:wl][:mysql][:driver]} /tmp
+    rm -rf /tmp/Worklight
+    unzip -q #{Chef::Config[:file_cache_path]}/#{filenameWLServer}
+    cd /opt/IBM/InstallationManager/eclipse
+    ./IBMIM --launcher.ini silent-install.ini -input /tmp/responseFile.xml -log /tmp/WLServer-${INSTDATE}.xml -acceptLicense
+    rm -rf /tmp/Worklight
+    rm /tmp/#{node[:wl][:mysql][:driver]}
+  EOH
+  not_if {File.exists?("/opt/IBM/Worklight")}
+end
+
+template "#{node[:wl][:tomcat][:installdir]}/conf/Catalina/localhost/worklight.xml" do
+  source "worklight.xml.erb"
+  owner "tomcat"
+  group "tomcat"
+  mode "0644"
+end
+
+directory "#{node[:wl][:tomcat][:installdir]}/webapps/worklight" do
+  owner "tomcat"
+  group "tomcat"
+  mode "0755"
+  action :create
+end
