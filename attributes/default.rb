278a7414db8c5355bd0911ab05cb55e4f4c436af
Satoshi Tanaka <kangae2@gmail.com>
2013-07-04 14:59:13 +0900

    update attribute parameters

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,10 +22,10 @@ default["wl"]["arch"] = "x86_64"
 default["wl"]["database"]["selection"] = "mysql"
 default["wl"]["database"]["preinstalled"] = "true"
 
-default["wl"]["mysql"]["host"] = "localhost"
+default["wl"]["mysql"]["host"] = node['ipaddress']
 default["wl"]["mysql"]["port"] = 3306
 default["wl"]["mysql"]["username"] = "worklight"
-default["wl"]["mysql"]["password2"] = "worklight"
+default["wl"]["mysql"]["password2"] = "password"
 default["wl"]["mysql"]["driver"] = "mysql-connector-java-5.1.25-bin.jar"
 default["wl"]["mysql"]["appcenter"]["username"] = "appcenter"
 default["wl"]["mysql"]["appcenter"]["password"] = "appcenter"
